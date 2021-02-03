f8ac3576150cef065642e35393d7fbb0fb5956cc
davidchanho <davidho.crd@gmail.com>
2021-02-02 20:32:47 -0500

    unique posts

--- server/db/seed/index.ts
+++ server/db/seed/index.ts
@@ -3,35 +3,44 @@ import _ from "lodash";
 import mongoose from "mongoose";
 import db from "../models";
 
-const PUBLICATIONS_TO_ADD = 20;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/medium", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+const seed = () =>
+  _.times(20, () => {
+    mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/medium", {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
 
-const newId = new mongoose.Types.ObjectId();
+    const newId = new mongoose.Types.ObjectId();
 
-const newPub = new db.Publication({
-  _id: newId,
-  name: _.capitalize(faker.lorem.words(3).replace(/\s/g, "")),
-  posts: [],
-});
+    const newPub = new db.Publication({
+      _id: newId,
+      name: _.capitalize(faker.lorem.words(3).replace(/\s/g, "")),
+      posts: [],
+    });
+    
+    const posts = _.times(5, () => {
+      const newPost = new db.Post({
+        publicationId: newId,
+        title: _.capitalize(faker.lorem.words(3)),
+        body: faker.lorem.paragraph(5),
+      });
+      newPost.save()
+      return newPost;
+    });
 
-const newPost = new db.Post({
-  publicationId: newId,
-  title: _.capitalize(faker.lorem.words(3)),
-  body: faker.lorem.paragraph(5),
-});
+    newPub.posts = posts;
 
-newPub.posts.push(newPost);
+    Promise.all([newPub.save()])
+      .then(() => {
+        console.log("seeding successful");
+        process.exit(0);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        process.exit(1);
+      });
+  });
 
-Promise.all([newPub.save(), newPost.save()]).then(() => {
-  console.log("seeding successful");
-  process.exit(0);
-})
-.catch((err) => {
-  console.log(err.message);
-  process.exit(1);
-});;
+seed();
