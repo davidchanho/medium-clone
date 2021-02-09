4c88671b0a09d5d0001ae6a8c7f193b077f00546
davidchanho <davidho.crd@gmail.com>
2021-02-09 14:52:34 -0500

    add change bookmark when clicked on

--- frontend/src/shared/bookmark/index.tsx
+++ frontend/src/shared/bookmark/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { BsBookmark } from "react-icons/bs";
+import { BsBookmark, BsBookmarkFill } from "react-icons/bs";
 import { PostProps } from "../../types";
 import { useBookmark } from "./useBookmark";
 
 function Bookmark({ post }: PostProps) {
-  const { onBookmarkPost } = useBookmark(post);
-  
-  return <BsBookmark onClick={onBookmarkPost} />;
+  const { isBookmark, onBookmarkPost } = useBookmark(post);
+
+  return isBookmark ? (
+    <BsBookmarkFill onClick={onBookmarkPost} />
+  ) : (
+    <BsBookmark onClick={onBookmarkPost} />
+  );
 }
 
 export default Bookmark;
--- frontend/src/shared/bookmark/useBookmark.ts
+++ frontend/src/shared/bookmark/useBookmark.ts
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react";
 import { useActions } from "../../hooks/useActions";
 import { IPost } from "../../types";
+import { useSelector } from "./../../hooks/useSelector";
+import { userSelectors } from "./../../store/selectors/index";
 
 export const useBookmark = (post: IPost) => {
   const { bookmarkPost } = useActions();
+  const [isBookmark, setBookmark] = useState<boolean>(false);
+  const { user } = useSelector(userSelectors);
+
+  useEffect(() => {
+    if (user.bookmarks.includes(post)) {
+      setBookmark(true);
+    }
+  }, [user.bookmarks]);
 
   const onBookmarkPost = () => {
     if (post._id) {
@@ -10,5 +21,5 @@ export const useBookmark = (post: IPost) => {
     }
   };
 
-  return { onBookmarkPost };
+  return { onBookmarkPost, isBookmark };
 };
