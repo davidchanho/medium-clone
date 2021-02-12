229cb3c55d8d6564b5a89eda1b1ed3569c065a51
davidchanho <davidho.crd@gmail.com>
2021-02-11 23:33:31 -0500

    change border price card

--- frontend/src/components/upgrade/PricingCard.tsx
+++ frontend/src/components/upgrade/PricingCard.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "react-bootstrap";
 import { PricingCardProps } from "./types";
 
-function PricingCard({freq, price}: PricingCardProps) {
+function PricingCard({ freq, price }: PricingCardProps) {
+  const [isHover, setHover] = useState<boolean>(false);
+
+  const onMouseEnter = () => {
+    setHover(true);
+  };
+
+  const onMouseLeave = () => {
+    setHover(false);
+  };
+
   return (
-    <Card border="success" className="border rounded w-50 my-2">
+    <Card
+      border={isHover ? "success" : ""}
+      className="border rounded w-50 my-2"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
       <Card.Body className="d-flex align-items-center">
-        <input type="radio" name="upgrade" id={freq} />
+        <input type="radio" name="upgrade" id={freq} className="mr-2" />
         <div className="ml-2">
           <p>{freq}</p>
           <p>{price}</p>
