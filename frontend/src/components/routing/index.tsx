88a131b6eb545803d8f153229464348741c8e958
davidchanho <33166931+davidchanho@users.noreply.github.com>
2021-05-08 06:11:22 -0400

    remove publication  bar

--- frontend/src/components/routing/index.tsx
+++ frontend/src/components/routing/index.tsx
@@ -1,6 +1,8 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import PageLoader from "../../pages/page-loader";
 import Template from "../../pages/template";
+
 const PostDetailsPage = lazy(() => import("../../pages/post-details-page"));
 const PostFormPage = lazy(() => import("../../pages/post-form-page"));
 const PostsPage = lazy(() => import("../../pages/posts-page"));
@@ -9,7 +11,7 @@ const ReadingListPage = lazy(() => import("../../pages/reading-list-page"));
 function Routing() {
   return (
     <Router>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<PageLoader />}>
         <Routes>
           <Route element={<Template />}>
             <Route path="/" element={<PostsPage />} />
